bf78f1b6cb289c5590c03d4d668cf127550d896f
Antonio Lorusso <mitictonylor@gmail.com>
2020-05-24 16:49:36 +0100

    add url for mongodb cluster

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const port = process.env.PORT
 app.use(parser.json());
 app.use(cors());
 
-MongoClient.connect('mongodb://localhost:27017')
+
+
+
+const uri = "mongodb+srv://admin:Quizza1quizza2quizza3@quizza-aiaib.mongodb.net/test?retryWrites=true&w=majority";
+
+MongoClient.connect(uri)
 .then((client) => {
   const db = client.db('hall_of_fame');
   const triviaCollection = db.collection('Seek_and_destroy');
