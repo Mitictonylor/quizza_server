6fc363f1dac37157c1c49942d326573683727c37
Antonio Lorusso <mitictonylor@gmail.com>
2020-05-26 09:56:12 +0100

    set port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
 const cors = require('cors');
 const parser = require('body-parser');
-// const port = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.use(parser.json());
 app.use(cors());
@@ -23,6 +23,6 @@ MongoClient.connect(uri,{ useUnifiedTopology: true })
 })
 .catch(console.error);
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log(`Listening on port ${this.address().port}`);
 });
